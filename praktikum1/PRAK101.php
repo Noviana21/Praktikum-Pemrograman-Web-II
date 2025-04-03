48a98fe859546f018a6bb8ce310ef2b2915b5148
Novi <136134651+Noviana21@users.noreply.github.com>
2025-04-03 16:33:27 +0800

    Add files via upload

---
+++ praktikum1/PRAK101.php
@@ -0,0 +1,4 @@
+<?php
+echo"Hello World in PHP<br>";
+echo "Nama Praktikan: Noviana Nur Aisyah<br>";
+echo "NIM: 2310817120005";
---
+++ praktikum1/PRAK102.php
@@ -0,0 +1,7 @@
+<?php 
+$jarijari = 4.2;
+$phi = 3.14;
+$volume = 4 / 3 * $phi * $jarijari * $jarijari * $jarijari;
+echo "Volume bola dengan jari-jari 4.2 m adalah: <br> ";
+echo number_format($volume, 3);
+echo " m3";
